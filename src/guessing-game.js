83c38caa16aab2255c12450fa1f81bb746b7fa94
inbox013 <inbox0130@gmail.com>
2022-08-15 10:25:59 +0300

    feat: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.maxim = max;
+    this.minim = min;
+  }
 
-    }
+  guess() {
+    this.middle = (this.maxim + this.minim) / 2;
+    return Math.round(this.middle);
+  }
 
-    guess() {
+  lower() {
+    this.maxim = Math.round(this.middle);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.minim = Math.round(this.middle);
+  }
 }
 
 module.exports = GuessingGame;
